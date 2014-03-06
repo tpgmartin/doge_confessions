ea5031d29920bcb9e79b745b51588a113921f10c
mantiscy <mantiscy@gmail.com>
2014-03-06 20:10:56 +0000

    Forgot these

--- doge_app/app/controllers/confessions_controller.rb
+++ doge_app/app/controllers/confessions_controller.rb
@@ -13,7 +13,31 @@ class ConfessionsController < ApplicationController
   end
 
   def create
-    UserMailer.submitted_confession_email(params[:email]).deliver
+    mail_num = %w(1 2 3 4 5 6 7 8 9 10).sample
+
+    case mail_num
+    when '1'
+      UserMailer.submitted_confession_email1(params[:email]).deliver
+    when '2'
+      UserMailer.submitted_confession_email2(params[:email]).deliver
+    when '3'
+      UserMailer.submitted_confession_email3(params[:email]).deliver
+    when '4'
+      UserMailer.submitted_confession_email4(params[:email]).deliver
+    when '5'
+      UserMailer.submitted_confession_email5(params[:email]).deliver
+    when '6'
+      UserMailer.submitted_confession_email6(params[:email]).deliver
+    when '7'
+      UserMailer.submitted_confession_email7(params[:email]).deliver
+    when '8'
+      UserMailer.submitted_confession_email8(params[:email]).deliver
+    when '9'
+      UserMailer.submitted_confession_email9(params[:email]).deliver
+    when '10'
+      UserMailer.submitted_confession_email10(params[:email]).deliver
+    end
+    
     redirect_to confessions_show_path
   end
 
